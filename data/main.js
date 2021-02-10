0accb71b47c84240af33034b56343034a61a7e24
Fabian Bucher <git@fabianbucher.de>
2021-02-10 16:53:25 +0100

    data: add automated data gathering
    * uses osmium with a node wrapper
    * extract data from .osm.pbf files provided from geofabrik.de
    * script and dependencies containerized

---
+++ data/main.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const osmium = require('osmium');
+
+const fileName = process.argv[2];
+if (!fileName) {
+  console.log('Please specify a .pbf file name.')
+  process.exit(1)
+}
+
+const pbf = new osmium.File(fileName);
+const reader = new osmium.Reader(pbf);
+const locationHandler = new osmium.LocationHandler();
+
+const streets = {};
+const acceptedStreetTypes = ['residential', 'primary', 'secondary', 'tertiary', 'unclassified'];
+
+const wayHandler = new osmium.Handler();
+wayHandler.on('way', (way) => {
+  const name = way.tags('name');
+  const streetType = way.tags('highway');
+  if (name !== undefined && streetType !== undefined && acceptedStreetTypes.includes(streetType)) {
+    const geoJSON = way.geojson();
+    if (streets[name]) {
+      streets[name].push(geoJSON);
+    } else {
+      streets[name] = [geoJSON];
+    }
+  }
+});
+
+wayHandler.on('after_ways', () => {
+  console.log(`saving ${Object.keys(streets).length} streets...`)
+  fs.writeFile('dist/result.json', JSON.stringify(streets, null, 2), 'utf8', (error) => {
+    if (error) {
+      throw error;
+    }
+    console.log(`results saved!`)
+  });
+  fs.writeFile('dist/result.min.json', JSON.stringify(streets), 'utf8', (error) => {
+    if (error) {
+      throw error;
+    }
+    console.log(`minified results saved!`)
+  });
+});
+
+osmium.apply(reader, locationHandler, wayHandler);
