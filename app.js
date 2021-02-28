e85f65d5a5e6b3c245f77167517b20e92c743f58
Fabian Bucher <git@fabianbucher.de>
2021-02-28 11:28:09 +0100

    add handlebars

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session')
 const app = express();
 const port = 8080;
+app.set('view engine', 'hbs');
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
@@ -25,4 +26,20 @@ app.get('*.html', function(req, res, next) {
     }
 });
 
-app.use(express.static('src'));
+app.get('/index.html', function(req, res, next) {
+    res.render('index', { username: req.session.username, layout: 'layout.hbs' })
+});
+
+app.get('/highscore.html', function(req, res, next) {
+    res.render('highscore', { username: req.session.username, layout: 'layout.hbs' })
+});
+
+app.get('/howto.html', function(req, res, next) {
+    res.render('howto', { username: req.session.username, layout: 'layout.hbs' })
+});
+
+app.get('/register.html', function(req, res, next) {
+    res.render('register', { username: req.session.username, layout: 'layout.hbs' })
+});
+
+app.use(express.static('public'));
