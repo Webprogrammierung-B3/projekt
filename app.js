6b48ffb5f3cbc30ce261f03f6a2d0a236fca0c4b
Schild, Paul <schild.paulschild@googlemail.com>
2021-03-01 15:15:08 +0100

    implement game

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const hbs = require('hbs');
-const streets = require('./data/dist/result.min.json')
+const streets = require('./data/dist/result.min.json');
+const haversine = require('haversine-distance');
+const {uuid} = require('uuidv4');
 const streetNames = Object.keys(streets);
 const len = streetNames.length;
 const app = express();
@@ -14,9 +16,11 @@ app.use(session({
     saveUninitialized: true,
     cookie: { secure: false }
 }))
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const blocks = {};
+const MAX_ROUNDS = 5;
+const GAMES = {};
 
 function getRandomStreet() {
     return streetNames[Math.floor(Math.random() * len)];
@@ -74,16 +78,78 @@ app.get('/game.html', function(req, res, next) {
 });
 
 app.get('/api/game', function(req, res, next) {
-    const responseJson = {
-        streetName: getRandomStreet(),
-        currentPoints: 0,
-        round: 1,
-        totalRounds: 5
-    };
-    req.session.currentGame = {
-        rounds: []
+    const streetName = getRandomStreet();
+    if (req.session.currentGame === undefined) {
+        req.session.currentGame = {
+            rounds: [],
+        }
     }
+    let currentPoints = 0;
+    for (const round of req.session.currentGame.rounds) {
+        currentPoints += round.points;
+    }
+    req.session.currentGame.rounds.push({streetName});
+    const responseJson = {
+        streetName,
+        currentPoints,
+        round: req.session.currentGame.rounds.length,
+        totalRounds: MAX_ROUNDS
+    };
     res.send(responseJson)
+});
+
+app.post('/api/game', function(req, res, next) {
+    console.log(req.body, 1);
+    const currentGame = req.session.currentGame;
+    const currentGuess = req.body;
+    const currentRoundIndex = currentGame.rounds.length - 1;
+    currentGame.rounds[currentRoundIndex].guess = currentGuess;
+    const streetPolygons = streets[currentGame.rounds[currentRoundIndex].streetName];
+
+    let shortestDistance;
+    let closestCoordinate;
+    for (const polygon of streetPolygons) {
+        for (const coordinate of polygon.coordinates) {
+            const distance = haversine(currentGuess, coordinate);
+            console.log(distance);
+            if (shortestDistance !== undefined) {
+                if (distance < shortestDistance) {
+                    shortestDistance = distance;
+                    closestCoordinate = coordinate;
+                }
+            } else {
+                shortestDistance = distance;
+                closestCoordinate = coordinate;
+            }
+        }
+    }
+    console.log("shortest Distance: ",shortestDistance);
+    currentGame.rounds[currentRoundIndex].distance = shortestDistance;
+    currentGame.rounds[currentRoundIndex].points = shortestDistance;
+    let currentPoints = 0;
+    for (const round of currentGame.rounds) {
+        currentPoints += round.points;
+    }
+    const responseJson = {
+        streetName: currentGame.rounds[currentRoundIndex].streetName,
+        streetPolygons,
+        closestCoordinate,
+        newPoints: currentGame.rounds[currentRoundIndex].points,
+        currentPoints,
+        round: currentRoundIndex + 1,
+        totalRounds: MAX_ROUNDS
+    };
+
+    if (currentRoundIndex + 1 === MAX_ROUNDS) {
+        const id = uuid();
+        currentGame.id = id;
+        GAMES[id] = currentGame;
+        req.session.currentGame = undefined;
+        responseJson.newGame = true;
+    }
+
+    res.send(responseJson)
+    console.log(req.body);
 });
 
 app.use(express.static('public'));
